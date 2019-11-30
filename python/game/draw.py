ffa3ce186cba062630b2261428f423901bb9b7af
domroutley <dominic.routley@gmail.com>
2019-11-30 20:59:05 +0000

    add base draw function

--- python/game/draw.py
+++ python/game/draw.py
@@ -1,4 +1,8 @@
 """
 Draws the board on the screen
 """
-pass
+from board import chess_board
+
+def draw(new_board):
+    if not isinstance(new_board, chess_board):
+        raise TypeError("Expected {} object, got {} object".format(chess_board, type(new_board)))
