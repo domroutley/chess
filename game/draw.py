a2d2082d68bbfa8e88817b4471cd2d847ee29933
domroutley <dominic.routley@gmail.com>
2019-12-02 20:56:54 +0000

    linting fixes

--- game/draw.py
+++ game/draw.py
@@ -3,6 +3,8 @@ Draws the board on the screen
 """
 from . import board
 
+
 def draw(new_board):
     if not isinstance(new_board, board.chess_board):
-        raise TypeError("Expected {} object, got {} object".format(board.chess_board, type(new_board)))
+        raise TypeError("Expected {} object, got {} object".format(board.chess_board,
+                                                                   type(new_board)))
